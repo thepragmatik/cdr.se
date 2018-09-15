b2a2ebd1a360b4e2846fa5a5db408a649532c422
Rahul Thakur <rahul.thakur.fullstack@gmail.com@>
2018-09-15 18:46:44 +1000

    o    cleanups

--- src/main/java/spike/cdr/store/MySQLCabTripStore.java
+++ src/main/java/spike/cdr/store/MySQLCabTripStore.java
@@ -1,7 +1,6 @@
 package spike.cdr.store;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -16,6 +15,8 @@ import javax.sql.DataSource;
  */
 public class MySQLCabTripStore implements CabTripStore {
 
+	private final String SQL_GET_CAB_TRIP_COUNT_PER_MEDALLION = "SELECT medallion, COUNT(*) FROM cab_trip_data WHERE DATE(pickup_datetime)='%s' AND medallion IN (%s) GROUP BY medallion";
+
 	private DataSource ds;
 
 	public MySQLCabTripStore(DataSource ds) {
@@ -26,101 +27,47 @@ public class MySQLCabTripStore implements CabTripStore {
 
 		List<CabTrip> result = new ArrayList<>();
 
-		ResultSet rs = null;
+		try (Connection connection = ds.getConnection();
+				ResultSet rs = connection.createStatement().executeQuery(SQL_GET_CAB_TRIP_COUNT_PER_MEDALLION);) {
 
-		try {
-			Connection connection = ds.getConnection();
+			int resultCount = 0;
 
-			PreparedStatement pstmt = connection.prepareStatement(
-					"SELECT medallion, COUNT(*) FROM cab_trip_data WHERE DATE(pickup_datetime)=? AND medallion IN ("
-							+ getValuePlaceholders(cabs) + ") GROUP BY medallion");
+			while (rs.next()) {
 
-			System.out.println(getValuePlaceholders(cabs));
+				String medallion = rs.getString(1);
 
-			pstmt.setObject(1, forDate.toString());
+				int count = rs.getInt(2);
 
-			int index = 2;
+				result.add(new CabTrip(medallion, count));
 
-			for (String cab : cabs) {
-				pstmt.setString(index++, cab);
+				resultCount++;
 			}
 
-			pstmt.setString(1, getMedallions(cabs));
-
-//			System.out.println(getMedallions(cabs));
-//
-			System.out.println(pstmt.toString());
-
-			boolean returnedResultset = pstmt.execute();
-
-			if (returnedResultset) {
-				rs = pstmt.getResultSet();
-
-				int resultCount = 0;
-
-				while (rs.next()) {
-
-					String medallion = rs.getString(1);
-
-					int count = rs.getInt(2);
-
-					result.add(new CabTrip(medallion, count));
-
-					resultCount++;
-				}
-
-				System.out.println(String.format("Added %d results", resultCount));
-
-			}
+			System.out.println(String.format("Added %d results", resultCount));
 
 		} catch (SQLException e) {
 
 			throw new StoreException("Error querying database.", e);
 
-		} finally {
-			if (null != rs) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-					System.err.println("Error closing resultset");
-					e.printStackTrace();
-
-				}
-			}
 		}
 
 		return result;
-	}
-
-	private String getValuePlaceholders(List<String> cabs) {
-		StringBuilder sb = new StringBuilder();
-
-		cabs.forEach((c) -> {
-
-			if (sb.length() > 0) {
-				sb.append(",");
-			}
-
-			sb.append("?");
-
-		});
-
-		return sb.toString();
 	}
 
 	private String getMedallions(List<String> cabs) {
 
 		StringBuilder sb = new StringBuilder();
 
-		cabs.forEach((c) -> {
+		cabs.forEach((medallion) -> {
 
 			if (sb.length() > 0) {
 				sb.append(",");
 			}
 
 			sb.append("'");
-			sb.append(c);
+			sb.append(medallion);
 			sb.append("'");
+
 		});
 
 		return sb.toString();
